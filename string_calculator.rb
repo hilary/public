0e8c6d43bf6d524a78b9c49a73f7f5dfec7de73d
Hilary Holz <hilary@hholz.com>
2014-01-27 16:19:54 -0800

    Step 1

---
+++ string_calculator.rb
@@ -0,0 +1,39 @@
+class StringCalculator
+
+  def Add(numbers)
+    numbers.split(',').map{|x| x.to_i}.reduce(0, :+)
+  end
+
+end
+
+require 'minitest/autorun'
+
+class TestStringCalculator < Minitest::Spec
+
+  before do
+    @calc = StringCalculator.new
+  end
+
+  describe 'an empty string' do
+    it 'returns 0' do
+      @calc.Add("").must_equal 0
+    end
+  end
+
+  describe 'a string with one number' do
+    it 'returns the number' do
+      @calc.Add("0").must_equal 0
+    end
+
+    it 'returns the number' do
+      @calc.Add("42").must_equal 42
+    end
+  end
+
+  describe 'a string with two numbers separated by a comma' do
+    it 'returns the sum' do
+      @calc.Add("210,12").must_equal 222
+    end
+  end
+
+end
